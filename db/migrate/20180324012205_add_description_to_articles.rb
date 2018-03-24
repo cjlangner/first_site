dcc53e49fceb941429e3d986c6f271e61f8d8dc7
CJLangner <cjlangner@protonmail.com>
2018-03-23 20:42:42 -0500

    Create articles table, model and add validations

---
+++ db/migrate/20180324012205_add_description_to_articles.rb
@@ -0,0 +1,7 @@
+class AddDescriptionToArticles < ActiveRecord::Migration[5.1]
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :added_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
